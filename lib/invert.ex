b4c695709237ebbd83b8ced09bff2b543a39b723
Faheetah <Faheetah@users.noreply.github.com>
2021-04-12 08:40:16 -0500

    Add cache

--- lib/invert.ex
+++ lib/invert.ex
@@ -2,12 +2,4 @@ defmodule Invert do
   @moduledoc """
   Documentation for `Invert`.
   """
-
-  def hello() do
-    "world"
-  end
-
-  def crash() do
-    raise "bye"
-  end
 end
--- lib/invert/application.ex
+++ lib/invert/application.ex
@@ -5,7 +5,9 @@ defmodule Invert.Application do
 
   @impl true
   def start(_type, _args) do
-    children = []
+    children = [
+      {Invert.Cache, Application.get_env(:invert, :tables)}
+    ]
 
     opts = [strategy: :one_for_one, name: Invert.Supervisor]
     Supervisor.start_link(children, opts)
