68e997483ebc42c49a2af70b974290ec4a07a763
Faheetah <Faheetah@users.noreply.github.com>
2021-04-12 12:33:42 -0500

    Generate unique words, write out unique words

--- lib/mix/tasks/generate_words.ex
+++ lib/mix/tasks/generate_words.ex
@@ -2,11 +2,14 @@ defmodule Mix.Tasks.Invert.GenerateWords do
   use Mix.Task
 
   @words 100
+  @max_words 5
 
   def run([num_words]) do
     File.write("test_data.txt",
       load_words()
       |> parse_words(String.to_integer(num_words))
+      |> Enum.sort
+      |> Enum.dedup
       |> Enum.join("\n")
     )
   end
@@ -14,10 +17,12 @@ defmodule Mix.Tasks.Invert.GenerateWords do
   def load_words() do
     case File.read("/usr/share/dict/words") do
       {:ok, f} ->
-        f
-        |> String.split("\n")
-        |> Enum.take_random(@words)
-        |> List.to_tuple
+        word_pool =
+          f
+          |> String.split("\n")
+          |> Enum.take_random(@words)
+        File.write("test_data_words.txt", Enum.join(word_pool, "\n"))
+        List.to_tuple(word_pool)
       {:error, e} ->
         IO.puts "Could not load word dictionary /usr/share/dict/words: #{e}"
         []
@@ -28,7 +33,7 @@ defmodule Mix.Tasks.Invert.GenerateWords do
     1..num_words
     |> Enum.map(fn _ ->
       term =
-        1..Enum.random(1..12)
+        1..Enum.random(1..@max_words)
         |> Enum.map(fn _ ->
           elem(words, Enum.random(0..@words - 1))
         end)
