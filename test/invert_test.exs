7824060c96b4fdce6bcb9b312578405e6d54f4e0
Faheetah <Faheetah@users.noreply.github.com>
2021-08-07 17:17:35 -0500

    Add test proving top result as expected

--- test/invert_test.exs
+++ test/invert_test.exs
@@ -1,4 +1,31 @@
 defmodule InvertTest do
   use ExUnit.Case
   doctest Invert
+
+  setup_all do
+    [
+      "beef",
+      "beef bar",
+      "beef bar in oil",
+      "bar of soap",
+      "saddle soap",
+      "beef jerky"
+    ]
+    |> Enum.with_index()
+    |> Enum.each(fn {entry, index} ->
+      entry
+      |> String.split(" ")
+      |> Enum.each(fn word ->
+        Invert.set(InvertTest, :name, {word, [entry, index]})
+      end)
+    end)
+  end
+
+  test "finds things" do
+    results =
+      Invert.get(InvertTest, :name, "beef bar oil")
+      |> then(fn {:ok, x} -> x end)
+
+    assert {["beef bar in oil", _], _} = hd(results)
+  end
 end
